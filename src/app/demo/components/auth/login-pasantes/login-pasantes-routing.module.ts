6fe1e1561db8d4275cf0a326cf12733467438648
Sua Euceda <suarubioeuceda@gmail.com>
2024-11-21 00:16:48 -0600

    feat(Registrar): Disenio)

--- src/app/demo/components/auth/login-pasantes/login-pasantes-routing.module.ts
+++ src/app/demo/components/auth/login-pasantes/login-pasantes-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { LoginPasantesComponent } from './login-pasantes.component';
+import { RegisterComponent } from '../register/register.component';
 
 @NgModule({
     imports: [RouterModule.forChild([
-        { path: '', component: LoginPasantesComponent }
+        { path: '', component: LoginPasantesComponent },
+        { path: 'register', component: RegisterComponent },
     ])],
     exports: [RouterModule]
 })
